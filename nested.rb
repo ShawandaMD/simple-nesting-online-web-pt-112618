c6f80b4df83ed8996bd0d4e68c2673d9478421dc
Shawanda Davis <shawandadavis93@gmail.com>
2018-12-09 04:58:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,12 +16,12 @@ def hopper
         }
      }
 
+programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+  programmer_hash[:alan_kay][:known_for]	   
 end
 
 def dennis_ritchies_language
@@ -55,15 +57,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     
+   programmer_hash[:dennis_ritchie][:languages].join() 
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,8 +77,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
-    
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
+    programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
 end
 
 def changing_alan
